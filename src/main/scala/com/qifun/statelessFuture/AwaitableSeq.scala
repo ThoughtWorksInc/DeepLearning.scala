a6ed6336fa2e31b1703c96ce67c8a892677d8a4d
杨博 <pop.atry@gmail.com>
2014-05-10 02:37:08 +0800

    Add macro to get simpler syntax

--- src/main/scala/com/qifun/statelessFuture/AwaitableSeq.scala
+++ src/main/scala/com/qifun/statelessFuture/AwaitableSeq.scala
@@ -5,10 +5,13 @@ import scala.collection.generic.CanBuildFrom
 import scala.collection.GenTraversableOnce
 import scala.collection.generic.CanCombineFrom
 import scala.collection.parallel.Combiner
+import scala.reflect.macros.Context
 
 object AwaitableSeq {
 
   import scala.language.implicitConversions
+
+  implicit def toUnderlyingSeq[A](awaitableSeq: AwaitableSeq[A, _]) = awaitableSeq.underlying
 
   implicit final class LinearSeqAsAwaitableSeq[A](underlying: LinearSeq[A]) {
     final def asFutureSeq = new AwaitableSeq[A, Unit](underlying)
@@ -40,13 +43,103 @@ object AwaitableSeq {
     final def asAwaitableSeq[TailRecResult] = new AwaitableSeq[A, TailRecResult](underlying: Generator[A]#Future[Unit])
   }
 
+  final def flatMapMacro(c: Context)(f: c.Expr[Nothing => Any]): c.Expr[Nothing] = {
+    import c.universe._
+
+    val Apply(TypeApply(Select(thisTree, _), List(b, _)), _) = c.macroApplication
+    val thisName = newTermName(c.fresh("yangBoAwaitableSeq"))
+    val thisVal = c.Expr(ValDef(Modifiers(), thisName, TypeTree(), thisTree))
+    val TypeRef(_, _, List(_, tailRecResultType)) = thisTree.tpe.widen
+    val functioExpr: c.Expr[Awaitable[Nothing, Nothing]] = f.tree match {
+      case fTree @ Function(vparams, body) => {
+        val trrTree = Select(Ident(thisName), newTypeName("TailRecResult"))
+        c.Expr(Apply(
+          Select(Ident(thisName), newTermName("awaitableFlatMap")),
+          List(Function(
+            vparams,
+            ANormalForm.applyMacroWithType(c)(
+              c.Expr(body),
+              AppliedTypeTree(Ident(typeOf[AwaitableSeq[_, _]].typeSymbol), List(b, trrTree)),
+              trrTree).tree))))
+      }
+      case fTree => {
+        c.error(fTree.pos, "Expcet function literal.")
+        reify { ??? }
+      }
+    }
+    reify {
+      thisVal.splice
+      functioExpr.splice.await
+    }
+  }
+
+  final def mapMacro(c: Context)(f: c.Expr[Nothing => Any]): c.Expr[Nothing] = {
+    import c.universe._
+    val Apply(TypeApply(Select(thisTree, _), List(b, _)), _) = c.macroApplication
+    val thisName = newTermName(c.fresh("yangBoAwaitableSeq"))
+    val thisVal = c.Expr(ValDef(Modifiers(), thisName, TypeTree(), thisTree))
+    val TypeRef(_, _, List(_, tailRecResultType)) = thisTree.tpe.widen
+    val functioExpr: c.Expr[Awaitable[Nothing, Nothing]] = f.tree match {
+      case fTree @ Function(vparams, body) => {
+        c.Expr(Apply(
+          Select(Ident(thisName), newTermName("awaitableMap")),
+          List(Function(
+            vparams,
+            ANormalForm.applyMacroWithType(c)(
+              c.Expr(body),
+              b,
+              Select(Ident(thisName), newTypeName("TailRecResult"))).tree))))
+      }
+      case fTree => {
+        c.error(fTree.pos, "Expcet function literal.")
+        reify { ??? }
+      }
+    }
+    reify {
+      thisVal.splice
+      functioExpr.splice.await
+    }
+  }
+
+  final def foreachMacro(c: Context)(f: c.Expr[Nothing => Any]): c.Expr[Unit] = {
+    import c.universe._
+    val Apply(TypeApply(Select(thisTree, _), List(u)), _) = c.macroApplication
+    val thisName = newTermName(c.fresh("yangBoAwaitableSeq"))
+    val thisVal = c.Expr(ValDef(Modifiers(), thisName, TypeTree(), thisTree))
+    val TypeRef(_, _, List(_, tailRecResultType)) = thisTree.tpe.widen
+    val functioExpr: c.Expr[Awaitable[Nothing, Nothing]] = f.tree match {
+      case fTree @ Function(vparams, body) => {
+        c.Expr(Apply(
+          Select(Ident(thisName), newTermName("awaitableForeach")),
+          List(Function(
+            vparams,
+            ANormalForm.applyMacroWithType(c)(
+              c.Expr(body),
+              u,
+              Select(Ident(thisName), newTypeName("TailRecResult"))).tree))))
+      }
+      case fTree => {
+        c.error(fTree.pos, "Expcet function literal.")
+        reify { ??? }
+      }
+    }
+    reify {
+      thisVal.splice
+      functioExpr.splice.await
+    }
+  }
+
 }
 
-final class AwaitableSeq[A, TailRecResult] private (underlying: LinearSeq[A]) {
+final class AwaitableSeq[A, TRR](val underlying: LinearSeq[A]) {
+
+  type TailRecResult = TRR
 
   private type Future[AwaitResult] = Awaitable[AwaitResult, TailRecResult]
 
-  final def foldRight[B](right: B)(converter: (A, B) => Future[B]): Future[B] = {
+  private type StatelessFuture[AwaitResult] = Awaitable[AwaitResult, TailRecResult]
+
+  final def foldRight[B](right: B)(converter: (A, B) => Future[B]): StatelessFuture[B] = {
     if (underlying.nonEmpty) {
       Awaitable[B, TailRecResult] {
         converter(underlying.head, new AwaitableSeq(underlying.tail).foldRight(right)(converter).await).await
@@ -56,7 +149,7 @@ final class AwaitableSeq[A, TailRecResult] private (underlying: LinearSeq[A]) {
     }
   }
 
-  final def foldLeft[B](left: B)(converter: (B, A) => Future[B]): Future[B] = {
+  final def foldLeft[B](left: B)(converter: (B, A) => Future[B]): StatelessFuture[B] = {
     if (underlying.nonEmpty) {
       Awaitable[B, TailRecResult] {
         new AwaitableSeq(underlying.tail).foldLeft(converter(left, underlying.head).await)(converter).await
@@ -66,29 +159,32 @@ final class AwaitableSeq[A, TailRecResult] private (underlying: LinearSeq[A]) {
     }
   }
 
-  final def flatMap[B, That](f: A => Future[That])(implicit toGenerator: That => Generator[B]#Future[Unit]) = Awaitable[Generator.Seq[B], TailRecResult] {
-    foldLeft(Generator[B].Future {}) { (left, current) =>
-      f(current).map { that =>
-        Generator[B].Future {
-          left.await
-          toGenerator(that).await
+  final def awaitableFlatMap[B, That](f: A => Future[AwaitableSeq[B, TailRecResult]]) = Awaitable[AwaitableSeq[B, TailRecResult], TailRecResult] {
+    new AwaitableSeq[B, TailRecResult](
+      foldLeft(Generator[B].Future {}) { (left, current) =>
+        f(current).map { that =>
+          Generator[B].Future {
+            left.await
+            Generator.linearSeqToFuture(that.underlying).await
+          }
         }
-      }
-    }.await
+      }.await)
   }
 
-  final def map[B, That](f: A => Future[B]) = Awaitable[Generator.Seq[B], TailRecResult] {
-    foldLeft(Generator[B].Future {}) { (left, current) =>
-      f(current).map { that =>
-        Generator[B].Future {
-          left.await
-          Generator[B].apply(that).await
-        }
-      }
-    }.await
+  final def awaitableMap[B, That](f: A => Future[B]) = Awaitable[AwaitableSeq[B, TailRecResult], TailRecResult] {
+    new AwaitableSeq[B, TailRecResult](
+      Generator.futureToGeneratorSeq(
+        foldLeft(Generator[B].Future {}) { (left, current) =>
+          f(current).map { that =>
+            Generator[B].Future {
+              left.await
+              Generator[B].apply(that).await
+            }
+          }
+        }.await))
   }
 
-  final def foreach[U](f: A => Future[U]) = Awaitable[Unit, TailRecResult] {
+  final def awaitableForeach[U](f: A => Future[U]) = Awaitable[Unit, TailRecResult] {
     foldLeft[Any](()) { (left, current) =>
       f(current)
     }.await
@@ -107,20 +203,10 @@ final class AwaitableSeq[A, TailRecResult] private (underlying: LinearSeq[A]) {
         }
       })
   }
-  //
-  //  final def withFilter(f: A => Future[Boolean]) = Awaitable[Generator.Seq[A], TailRecResult] {
-  //    foldLeft(Generator[A].Future {}) { (left, current) =>
-  //      f(current).map { that =>
-  //        if (that) {
-  //          Generator[A].Future {
-  //            left.await
-  //            Generator[A].apply(current).await
-  //          }
-  //        } else {
-  //          left
-  //        }
-  //      }
-  //    }.await
-  //  }
 
+  import scala.language.experimental.macros
+
+  final def flatMap[B, That](f: A => AwaitableSeq[B, TailRecResult]): AwaitableSeq[B, TailRecResult] = macro AwaitableSeq.flatMapMacro
+  final def map[B, That](f: A => B): AwaitableSeq[B, TailRecResult] = macro AwaitableSeq.mapMacro
+  final def foreach[U](f: A => U): Unit = macro AwaitableSeq.foreachMacro
 }
--- src/test/scala/com/qifun/statelessFuture/AwaitableSeqTest.scala
+++ src/test/scala/com/qifun/statelessFuture/AwaitableSeqTest.scala
@@ -9,8 +9,7 @@ class AwaitableSeqTest {
   def `simple yield`() {
     val l = List(1, 2, 3)
     val f = Future {
-      (for (i <- l.asFutureSeq) yield Future {
-
+      for (i <- l.asFutureSeq) yield {
         val sb = Seq.newBuilder[Int]
         sb += i
         Future {
@@ -18,11 +17,13 @@ class AwaitableSeqTest {
         }.await
         sb += i
         sb.result
-      }).await
+      }
     }
     implicit def catcher = PartialFunction.empty
     f.foreach { seq =>
-      assertEquals(seq, Seq(Seq(1, 99, 1), Seq(2, 99, 2), Seq(3, 99, 3)))
+      assertEquals(
+        Seq(Seq(1, 99, 1), Seq(2, 99, 2), Seq(3, 99, 3)),
+        seq.underlying)
     }
   }
 
@@ -32,25 +33,28 @@ class AwaitableSeqTest {
     val l2 = Vector(4, 5, 6)
     val l3 = Array(7, 8)
     val f = Future {
-      (for {
+      for {
         i <- l1.asFutureSeq
         if i != 2
         j <- l2.asFutureSeq
+        d = j + i
         k <- l3.asFutureSeq
-      } yield Future {
+      } yield {
         val sb = Seq.newBuilder[Int]
         sb += i
         Future {
           sb += 99
         }.await
-        sb += j
+        sb += d
         sb += k
         sb.result
-      }).await
+      }
     }
     implicit def catcher = PartialFunction.empty
     f.foreach { seq =>
-      assertEquals(seq, Seq(Seq(1, 99, 4, 7), Seq(1, 99, 4, 8), Seq(1, 99, 5, 7), Seq(1, 99, 5, 8), Seq(1, 99, 6, 7), Seq(1, 99, 6, 8), Seq(3, 99, 4, 7), Seq(3, 99, 4, 8), Seq(3, 99, 5, 7), Seq(3, 99, 5, 8), Seq(3, 99, 6, 7), Seq(3, 99, 6, 8)))
+      assertEquals(
+        Seq(Seq(1, 99, 5, 7), Seq(1, 99, 5, 8), Seq(1, 99, 6, 7), Seq(1, 99, 6, 8), Seq(1, 99, 7, 7), Seq(1, 99, 7, 8), Seq(3, 99, 7, 7), Seq(3, 99, 7, 8), Seq(3, 99, 8, 7), Seq(3, 99, 8, 8), Seq(3, 99, 9, 7), Seq(3, 99, 9, 8)),
+        seq.underlying)
     }
 
   }
@@ -60,13 +64,13 @@ class AwaitableSeqTest {
     val sb = Seq.newBuilder[Int]
     val l = List(1, 2, 3)
     val f = Future {
-      (for (i <- l.asFutureSeq) Future {
+      for (i <- l.asFutureSeq) {
         sb += i
         Future {
           sb += 99
         }.await
         sb += i
-      }).await
+      }
     }
     implicit def catcher = PartialFunction.empty
     f.foreach { u =>
@@ -81,19 +85,19 @@ class AwaitableSeqTest {
     val l2 = Vector(4, 5, 6)
     val l3 = Array(7, 8)
     val f = Future {
-      (for {
+      for {
         i <- l1.asFutureSeq
         if i != 2
         j <- l2.asFutureSeq
         k <- l3.asFutureSeq
-      } Future {
+      } {
         sb += i
         Future {
           sb += 99
         }.await
         sb += j
         sb += k
-      }).await
+      }
     }
     implicit def catcher = PartialFunction.empty
     f.foreach { u =>
