d35258c8c6db583a3f0b7422e051a90cb80a9d4e
杨博 <pop.atry@gmail.com>
2014-06-04 14:34:58 +0800

    Add new future: JumpInto

---
+++ src/main/scala/com/qifun/statelessFuture/JumpInto.scala
@@ -0,0 +1,21 @@
+package com.qifun.statelessFuture
+
+import scala.util.control.Exception.Catcher
+import scala.util.control.TailCalls._
+import java.util.concurrent.Executor
+
+/**
+ * Let the code after [[JumpInto#await]] run in `executor`.
+ * @param executor Where the code after [[JumpInto#await]] run.
+ */
+final case class JumpInto[TailRecResult](executor: Executor) extends Future.Stateless[Unit] {
+
+  override final def onComplete(handler: Unit => TailRec[Unit])(implicit catcher: Catcher[TailRec[Unit]]): TailRec[Unit] = {
+    done(executor.execute(new Runnable {
+      override final def run(): Unit = {
+        handler(()).result
+      }
+    }))
+  }
+
+}
