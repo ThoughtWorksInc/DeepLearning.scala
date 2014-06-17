7d1a29b277e05d0c08f6b9409ed7b10164d4c974
杨博 <pop.atry@gmail.com>
2014-06-17 10:54:47 +0800

    Replace Interruptor with Poll

---
+++ src/main/scala/com/qifun/statelessFuture/Poll.scala
@@ -0,0 +1,34 @@
+package com.qifun.statelessFuture
+
+import java.util.concurrent.atomic.AtomicReference
+import scala.util.control.Exception.Catcher
+import scala.util.control.TailCalls.TailRec
+import scala.util.Try
+
+object Poll {
+  /**
+   * Returns a [[PollFuture]] that completes when any one of the `futures` completes,
+   * and fails when any one of the `futures` fails,
+   */
+  def apply[AwaitResult](futures: CancelableFuture[AwaitResult]*) = {
+    val result = new Poll[AwaitResult](new AtomicReference(Left(({ () =>
+      for (future <- futures) {
+        future.cancel()
+      }
+    }, Nil))))
+    for (future <- futures) {
+      result.tryCompleteWith(future)
+    }
+    result
+  }
+}
+
+/**
+ * A [[CancelableFuture]] composed of some sub-futures.
+ *
+ * This [[CancelableFuture]] completes when any one of the sub-futures completes,
+ * and fails when any one of the sub-futures fails,
+ */
+final class Poll[AwaitResult] private (
+  val stateReference: AtomicReference[Either[(() => Unit, List[(AwaitResult => TailRec[Unit], Catcher[TailRec[Unit]])]), Try[AwaitResult]]])
+  extends AnyVal with CancelableFuture[AwaitResult]
