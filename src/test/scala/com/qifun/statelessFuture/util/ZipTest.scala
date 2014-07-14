3cba86c53f915a8f2f3d42da86d1a98bc36d4b0f
zxiy <zxiuyu@126.com>
2014-07-14 15:16:55 +0800

    修正了JumpInto调用位置的错误导致Zip在一起的两个Future先后执行

--- src/test/scala/com/qifun/statelessFuture/util/ZipTest.scala
+++ src/test/scala/com/qifun/statelessFuture/util/ZipTest.scala
@@ -8,39 +8,43 @@ import org.junit.Assert._
 class ZipTest {
   @Test
   def `zipTest1`() {
-	var future1Counter:Int = 0;
-	var future2Counter:Int = 0;
-	val executor = Executors.newCachedThreadPool()
-	
-	val myFuture:Future.Stateless[Unit] = Future[Unit] {
-	  
-	  JumpInto(executor).await
-	  val future1: Future.Stateful[Unit] = Promise.completeWith(Future[Unit] {
-	    Future {
-        for (i <- 1 to 500000) {
-          future1Counter = i;
-        }
-      }.await
-	  })
-	  
-	  JumpInto(executor).await
-	  val future2: Future.Stateful[Unit] = Promise.completeWith(Future[Unit] {
-	    Future {
-        for (i <- 1 to 800000) {
-          future2Counter = i;
-        }
-      }.await
-	  })
-	  
-	  val futureZip = new Zip(future1, future2)
-	  futureZip.await
-	  println("FutureZip process successful")
-	}
-	
-	Blocking.blockingAwait(myFuture)
-	
-	println(s"after FutureZip await, future1Counter = $future1Counter, future2Counter = $future2Counter")
-	assertEquals(future1Counter, 500000)
-	assertEquals(future2Counter, 800000)
+    @volatile var future1Counter: Int = 0;
+    @volatile var future2Counter: Int = 0;
+    val executor = Executors.newCachedThreadPool()
+
+    val myFuture: Future.Stateless[Unit] = Future[Unit] {
+
+      
+      val future1: Future.Stateful[Unit] = Promise.completeWith(Future[Unit] {
+        JumpInto(executor).await
+        Future {
+          println(System.currentTimeMillis())
+          for (i <- 1 to 500000) {
+            future1Counter = i;
+          }
+        }.await
+      })
+
+      
+      val future2: Future.Stateful[Unit] = Promise.completeWith(Future[Unit] {
+        JumpInto(executor).await
+        Future {
+          println(System.currentTimeMillis())
+          for (i <- 1 to 800000) {
+            future2Counter = i;
+          }
+        }.await
+      })
+
+      val futureZip = new Zip(future1, future2)
+      futureZip.await
+      println("FutureZip process successful")
+    }
+
+    Blocking.blockingAwait(myFuture)
+
+    println(s"after FutureZip await, future1Counter = $future1Counter, future2Counter = $future2Counter")
+    assertEquals(future1Counter, 500000)
+    assertEquals(future2Counter, 800000)
   }
 }
