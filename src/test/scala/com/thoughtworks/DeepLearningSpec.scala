2e84477bd5dfc8a8ae91a4b617ed6553a117d872
Yang Bo <atryyang@thoughtworks.com>
2016-09-09 14:39:58 +0800

    Add a complete test case

--- src/test/scala/com/thoughtworks/DeepLearningSpec.scala
+++ src/test/scala/com/thoughtworks/DeepLearningSpec.scala
@@ -11,65 +11,36 @@ import shapeless.DepFn1
 final class DeepLearningSpec extends FreeSpec with Matchers with Inside {
 
   "-" in {
-    //    import DeepLearning._
-
-    //    val input = Input[scala.Double, scala.Double]
-    //    val output = input - Double(3.0)
-    //
-
     implicit def learningRate = new DeepLearning.LearningRate {
-      def apply() = 0.0003
+      def apply() = 0.03
     }
 
     def f(dsl: Dsl)(input: dsl.Double): dsl.Double = {
       import dsl._
       -input - Double.weight(3.0)
     }
+    def g(dsl: Dsl)(input: dsl.Double): dsl.Double = {
+      import dsl._
+      input - Double.weight(1.2)
+    }
 
     val id = new Id[scala.Double, scala.Double]
     val dsl = new DeepLearning[id.Input]
     val f1 = f(dsl)(id)
-    val nn = f1.compose(f1)
+    val g1 = g(dsl)(id)
+    val nn = f1.compose(g1)
+    def train(input: scala.Double): scala.Double = {
+      val output = nn.forward(dsl.Double(10.11))
+      output.backward(output.value)
+      output.value
+    }
+    val initialLoss = train(10.11)
+    for (_ <- 0 until 100) {
+      train(10.11)
+    }
+    math.abs(train(10.11)) should be < math.abs(initialLoss)
 
-    f1.forward(dsl.Double(4.5))
-
-
-    //    def xxx[Input0 >: DeepLearning.Cache.Aux[Input0, scala.Double, scala.Double]] = {
-    //
-    //      val dsl = new DeepLearning[Input0]
-    //      val f1: Differentiable.Aux[Input0, Cache.Aux[Input0, scala.Double, scala.Double]] = f(dsl)(new Id[scala.Double, scala.Double])
-    //
-    //      f1.compose(f1)
-    //    }
-    //
-    //    val f1 = xxx[T forSome {type T <: DeepLearning.Cache.Aux[T, scala.Double, scala.Double]}]
-    //
-    //    type Input = T forSome {type T <: DeepLearning.Cache.Aux[T, scala.Double, scala.Double]}
-    //    val dsl = new DeepLearning[Input]
-    //    val f1 = f(dsl)((new Id[Input]).self)
-
-//        DeepLearning.Differentiable.compose(f1,f1)
-    //    f1.compose(f1)
-
-    //    implicitly[Cache.Aux[Cache, scala.Double, scala.Double] <:< Cache.Aux[Cache.Aux[Cache, scala.Double, scala.Double], scala.Double, scala.Double]]
-
-
-    //    DeepLearning.Differentiable (new DepFn1 {
-    //
-    //    })
-    //
-    //    DeepLearning.Differentiable { dsl: DeepLearning[DeepLearning.DoubleCache.Aux[DeepLearning.DoubleCache]] =>
-    //      import dsl._
-    //      input: Double =>
-    //        dsl.doubleOps(input) - Double(3.0)
-    //    }
-    //    val output = Differentiable { input =>
-    //      input - Double(3.0)
-    //    }
-
-
-    //    network.forwardApply(Double(2.9))
-
+    math.abs(train(10.11)) should be < 0.5
   }
 
 }
