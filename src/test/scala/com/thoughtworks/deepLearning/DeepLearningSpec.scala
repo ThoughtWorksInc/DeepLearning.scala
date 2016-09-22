cc75a10a8017f161bffc13d87380c97c3e318261
Yang Bo <atryyang@thoughtworks.com>
2016-09-22 14:34:25 +0800

    Fix compilation error for tests

--- src/test/scala/com/thoughtworks/deepLearning/DeepLearningSpec.scala
+++ src/test/scala/com/thoughtworks/deepLearning/DeepLearningSpec.scala
@@ -1,10 +1,6 @@
 package com.thoughtworks.deepLearning
 
-import cats.Eval
-import com.thoughtworks.deepLearning.Differentiable.Batch
-import com.thoughtworks.deepLearning.Differentiable.DifferentiableArray2D.Array2DLiteral
-import com.thoughtworks.deepLearning.Dsl.DslFunction
-import org.nd4j.linalg.api.ndarray.INDArray
+import com.thoughtworks.deepLearning.Differentiable.{SymbolicInput}
 import org.scalatest.{FreeSpec, Matchers}
 
 /**
@@ -15,34 +11,23 @@ final class DeepLearningSpec extends FreeSpec with Matchers {
   implicit def learningRate = new Differentiable.LearningRate {
     def apply() = 0.03
   }
+
   "XOR" in {
-
-    final case class PredictXor[D <: Dsl](dsl: D) extends DslFunction with DeepLearning {
-
+    def predictXor(dsl: Dsl)(in: dsl.Array2D) = {
       import dsl._
-
-      override type In = Array2D
-      override type Out = Array2D
-      //
-      //      private def loss(likelihood: Array2D, expectedLabels: Array2D) = {
-      //        -(log(likelihood) * expectedLabels).reduceSum
-      //      }
-
-      override def apply(in: In): Out = {
-        sigmoid(fullyConnectedThenRelu(
-          (0 until 10).foldLeft(fullyConnectedThenRelu(in, 2, 50)) { (hiddenLayer, _) =>
-            fullyConnectedThenRelu(hiddenLayer, 50, 50)
-          },
-          50,
-          2
-        ))
-
-      }
-
+      val deepLearning = DeepLearning(dsl)
+      import deepLearning._
+      sigmoid(fullyConnectedThenRelu(
+        (0 until 10).foldLeft(fullyConnectedThenRelu(in, 2, 50)) { (hiddenLayer, _) =>
+          fullyConnectedThenRelu(hiddenLayer, 50, 50)
+        },
+        50,
+        2
+      ))
     }
 
-
-    val predictor = Differentiable.fromDsl[PredictXor]
+    val symbolicInput = shapeless.the[SymbolicInput {type InputSymbol[D <: Dsl] = D#Array2D}]
+    val predictor = predictXor(symbolicInput.dsl)(symbolicInput.inputSymbol)
   }
 
 }
