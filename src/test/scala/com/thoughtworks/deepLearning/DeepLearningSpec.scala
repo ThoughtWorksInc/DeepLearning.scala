2c520563d5c375c615c11bcc3c502f174c5092ee
Yang Bo <atryyang@thoughtworks.com>
2016-09-23 13:41:22 +0800

    Add train function for test

--- src/test/scala/com/thoughtworks/deepLearning/DeepLearningSpec.scala
+++ src/test/scala/com/thoughtworks/deepLearning/DeepLearningSpec.scala
@@ -1,6 +1,8 @@
 package com.thoughtworks.deepLearning
 
-import com.thoughtworks.deepLearning.Differentiable.{SymbolicDsl, SymbolicInput}
+import cats.Eval
+import com.thoughtworks.deepLearning.Differentiable.{Batch, SymbolicDsl, SymbolicInput}
+import org.nd4j.linalg.api.ndarray.INDArray
 import org.scalatest.{FreeSpec, Matchers}
 
 /**
@@ -13,7 +15,7 @@ final class DeepLearningSpec extends FreeSpec with Matchers {
   }
 
   "XOR" in {
-    def predictXor(dsl: Dsl)(input: dsl.Array2D):dsl.Array2D = {
+    def predictXor(dsl: SymbolicDsl)(input: dsl.Array2D): dsl.Array2D = {
       import dsl._
       val deepLearning = DeepLearning(dsl)
       import deepLearning._
@@ -27,18 +29,31 @@ final class DeepLearningSpec extends FreeSpec with Matchers {
     }
 
     val predictorInput = shapeless.the[SymbolicInput {type Ast[D <: SymbolicDsl] = D#Array2D}]
-    val predictor = predictXor(predictorInput.dsl)(predictorInput.ast)
+    val predictor: Differentiable.Aux[Batch.Aux[Eval[INDArray], Eval[Option[INDArray]]], Batch.Aux[Eval[INDArray], Eval[Option[INDArray]]]] = predictXor(predictorInput.dsl)(predictorInput.ast).underlying
 
-    type LossInput[D <: Dsl] = D# ::[(D#Array2D), (D# ::)[(D#Array2D), (D#HNil)]]
-    def loss(dsl: Dsl)(scoresAndLabels: LossInput[dsl.type]) = {
-      val scores = scoresAndLabels.head
-      val labels = scoresAndLabels.tail.head
+    type Array2DPair[D <: Dsl] = D# ::[(D#Array2D), (D# ::)[(D#Array2D), (D#HNil)]]
+    def loss(dsl: SymbolicDsl)(scoresAndLabels: Array2DPair[dsl.type]) = {
+      import dsl._
+      val likelihood = scoresAndLabels.head
+      val expectedLabels = scoresAndLabels.tail.head
+      -(log(likelihood) * expectedLabels).reduceSum
     }
 
     // Does not compile because Scala compile is not able to search the implicit value with very complex dependent type
-    //    val lossInput = shapeless.the[SymbolicInput {type Ast[D <: SymbolicDsl] = LossInput[D]}]
+    // val lossInput = shapeless.the[SymbolicInput {type Ast[D <: SymbolicDsl] = Array2DPair[D]}]
+
     val lossInput = SymbolicInput.hconsInput(learningRate, SymbolicInput.array2DInput, SymbolicInput.hconsInput(learningRate, SymbolicInput.array2DInput, SymbolicInput.hnilInput))
-    val lossNetwork = loss(lossInput.dsl)(lossInput.ast)
+    val lossNetwork = loss(lossInput.dsl)(lossInput.ast).underlying
+
+    def train(dsl: SymbolicDsl)(inputAndLabels: Array2DPair[dsl.type]) = {
+      import dsl._
+
+      val input = inputAndLabels.head
+      val expectedLabels = inputAndLabels.tail.head
+      val likelihood = RichDifferentiable(predictor).apply(input)
+      RichDifferentiable(lossNetwork)(::(Array2D, ::(Array2D, HNil)), Double)((likelihood :: (expectedLabels :: (HNil: HNil)) (Array2D, HNil)) (dsl.Array2D, ::(Array2D, HNil)))
+    }
+
   }
 
 }
