cb565767f3dda8c151b9de2ea0b99422e0365979
Yang Bo <atryyang@thoughtworks.com>
2016-10-25 17:19:58 +0800

    Remove an unused import

--- differentiable/src/test/scala/com/thoughtworks/deepLearning/DifferentiableSpec.scala
+++ differentiable/src/test/scala/com/thoughtworks/deepLearning/DifferentiableSpec.scala
@@ -5,7 +5,6 @@ import com.thoughtworks.deepLearning.Differentiable.LearningRate
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4s.Implicits._
 import org.scalatest._
-import resource._
 
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
