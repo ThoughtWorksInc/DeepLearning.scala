f128ee1a7c4175badb97a453ec392670dc239618
Yang Bo <atryyang@thoughtworks.com>
2016-10-31 15:58:49 +0800

    Give Compose meaningful name

--- differentiable/src/main/scala/com/thoughtworks/deepLearning/any/ast/Compose.scala
+++ differentiable/src/main/scala/com/thoughtworks/deepLearning/any/ast/Compose.scala
@@ -5,12 +5,14 @@ import com.thoughtworks.deepLearning.{Batch, Ast}
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
-final case class Compose[A <: Batch, B <: Batch, C <: Batch](leftOperand: Ast.Aux[B, C], rightOperand: Ast.Aux[A, B])
+final case class Compose[Input0 <: Batch, Temporary <: Batch, Output0 <: Batch](
+    leftOperand: Ast.Aux[Temporary, Output0],
+    rightOperand: Ast.Aux[Input0, Temporary])
     extends Ast {
-  override type Input = A
-  override type Output = C
+  override type Input = Input0
+  override type Output = Output0
 
-  override def forward(input: A): C = {
+  override def forward(input: Input0): Output0 = {
     leftOperand.forward(rightOperand.forward(input))
   }
 }
