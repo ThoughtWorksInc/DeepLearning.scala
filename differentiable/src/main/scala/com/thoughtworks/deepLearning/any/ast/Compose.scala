9baf2026351a4bd7df68ba0588ca2de2320f5746
Yang Bo <atryyang@thoughtworks.com>
2016-11-02 09:54:20 +0800

    Update indention

--- differentiable/src/main/scala/com/thoughtworks/deepLearning/any/ast/Compose.scala
+++ differentiable/src/main/scala/com/thoughtworks/deepLearning/any/ast/Compose.scala
@@ -8,13 +8,13 @@ import com.thoughtworks.deepLearning.{Batch, Ast}
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
 final case class Compose[Input0 <: Batch, Temporary <: Batch, Output0 <: Batch](
-                                                                                 leftOperand: WidenAst[Temporary, Output0],
-                                                                                 rightOperand: WidenAst[Input0, Temporary])
+    leftOperand: WidenAst[Temporary, Output0],
+    rightOperand: WidenAst[Input0, Temporary])
     extends Ast {
   override type Input = Input0
   override type Output = Output0
 
-  override def forward(input: Input0): Output0 = {
+  override def forward(input: Input): Output = {
     leftOperand.forward(rightOperand.forward(input))
   }
 }
