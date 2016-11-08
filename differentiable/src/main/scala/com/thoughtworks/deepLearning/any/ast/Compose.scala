eb84d1aa55fc60c5a71df9b6ac5161227935a534
Yang Bo <atryyang@thoughtworks.com>
2016-11-09 00:30:47 +0800

    Adjust indention

--- differentiable/src/main/scala/com/thoughtworks/deepLearning/any/ast/Compose.scala
+++ differentiable/src/main/scala/com/thoughtworks/deepLearning/any/ast/Compose.scala
@@ -8,8 +8,8 @@ import com.thoughtworks.deepLearning.{Differentiable, DifferentiableFunction}
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
 final case class Compose[Input0 <: Differentiable, Temporary <: Differentiable, Output0 <: Differentiable](
-                                                                                 leftOperand: DifferentiableFunction.Ast[Temporary, Output0],
-                                                                                 rightOperand: DifferentiableFunction.Ast[Input0, Temporary])
+    leftOperand: DifferentiableFunction.Ast[Temporary, Output0],
+    rightOperand: DifferentiableFunction.Ast[Input0, Temporary])
     extends DifferentiableFunction {
   override type Input = Input0
   override type Output = Output0
