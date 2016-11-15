a68d833319f1a319da76b57ee84d15d07f0cea98
Yang Bo <atryyang@thoughtworks.com>
2016-11-15 15:45:41 +0800

    Format code

--- differentiable/src/main/scala/com/thoughtworks/deepLearning/any/ast/Compose.scala
+++ differentiable/src/main/scala/com/thoughtworks/deepLearning/any/ast/Compose.scala
@@ -8,8 +8,8 @@ import com.thoughtworks.deepLearning.{Batch, NeuralNetwork}
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
 final case class Compose[Input0 <: Batch, Temporary <: Batch, Output0 <: Batch](
-                                                                                                            leftOperand: NeuralNetwork.Aux[Temporary, Output0],
-                                                                                                            rightOperand: NeuralNetwork.Aux[Input0, Temporary])
+    leftOperand: NeuralNetwork.Aux[Temporary, Output0],
+    rightOperand: NeuralNetwork.Aux[Input0, Temporary])
     extends NeuralNetwork {
   override type Input = Input0
   override type Output = Output0
