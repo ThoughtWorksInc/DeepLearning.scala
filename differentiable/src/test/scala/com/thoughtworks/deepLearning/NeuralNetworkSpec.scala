b83b2f6233c41667eacd4e03c5b7b60fe372f45c
Yang Bo <atryyang@thoughtworks.com>
2016-11-16 10:39:57 +0800

    Better naming

--- differentiable/src/test/scala/com/thoughtworks/deepLearning/NeuralNetworkSpec.scala
+++ differentiable/src/test/scala/com/thoughtworks/deepLearning/NeuralNetworkSpec.scala
@@ -12,7 +12,7 @@ import org.nd4s.Implicits._
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
-final class DifferentiableSpec extends FreeSpec with Matchers {
+final class NeuralNetworkSpec extends FreeSpec with Matchers {
 
   implicit val learningRate = new LearningRate {
     override def apply() = 0.0003
