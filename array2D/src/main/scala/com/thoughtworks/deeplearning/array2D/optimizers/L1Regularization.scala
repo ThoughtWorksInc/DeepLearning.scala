14afe9ba14f1cc2e22e3155ef44d21335f611b57
Yang Bo <atryyang@thoughtworks.com>
2016-11-23 18:05:37 +0800

    L1 regularization should increase the value when a weight is negative

--- array2D/src/main/scala/com/thoughtworks/deeplearning/array2D/optimizers/L1Regularization.scala
+++ array2D/src/main/scala/com/thoughtworks/deeplearning/array2D/optimizers/L1Regularization.scala
@@ -2,6 +2,7 @@ package com.thoughtworks.deeplearning.array2D.optimizers
 
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4s.Implicits._
+import org.nd4j.linalg.ops.transforms.Transforms._
 
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
@@ -10,7 +11,7 @@ trait L1Regularization extends LearningRate {
   protected def l1Regularization: scala.Double
 
   override def updateNDArray(oldValue: INDArray, delta: INDArray): INDArray = {
-    super.updateNDArray(oldValue, delta) - l1Regularization * currentLearningRate()
+    super.updateNDArray(oldValue, delta) - sign(oldValue) * l1Regularization * currentLearningRate()
   }
 
 }
