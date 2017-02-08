95b59c37c8d9db263d3642dd9938fbcdc69fe9f5
Yang Bo <atryyang@thoughtworks.com>
2017-02-08 11:54:53 +0800

    Support packed and unpacked values

--- OpenCL/src/main/scala/com/thoughtworks/deeplearning/OpenCL.scala
+++ OpenCL/src/main/scala/com/thoughtworks/deeplearning/OpenCL.scala
@@ -4,6 +4,7 @@ import java.util
 
 import com.dongxiguo.fastring.Fastring
 import com.dongxiguo.fastring.Fastring.Implicits._
+import com.thoughtworks.deeplearning.OpenCL.DslFunction.Value
 import shapeless.{HList, HNil}
 
 import scala.collection.JavaConverters._
@@ -16,8 +17,8 @@ object OpenCL {
 
   trait Context {
     def freshName(prefix: String): String
-    def getValue(dslFunction: DslFunction[_, _]): Fastring
-    def getType(dslType: DslType[_]): Fastring
+    def getValue(dslFunction: DslFunction[_, _]): Value
+    def getPackedType(dslType: DslType[_]): Fastring
   }
 
   final case class Kernel[Input <: HList, Output](name: String,
@@ -33,7 +34,7 @@ object OpenCL {
       seed += 1
       id
     }
-    val types = mutable.Map.empty[String, Fastring]
+    val types = mutable.Set.empty[Seq[String]]
     val globalDeclarations = mutable.Buffer.empty[Fastring]
     val globalDefinitions = mutable.Buffer.empty[Fastring]
 
@@ -42,22 +43,29 @@ object OpenCL {
     } yield {
       val localDefinitions = mutable.Buffer.empty[Fastring]
 
-      val functionCodeCache = new util.IdentityHashMap[DslFunction[_, _], Fastring]().asScala
+      val functionCodeCache = new util.IdentityHashMap[DslFunction[_, _], Value]().asScala
 
       val functionContext = new Context {
-        override def getType(dslType: DslType[_]): Fastring = {
-          val code = dslType.toCode(this)
-          types.get(code.identifier) match {
-            case Some(typeName) => typeName
-            case None =>
-              globalDeclarations += code.globalDeclarations
-              globalDefinitions += code.globalDefinitions
-              types.put(code.identifier, code.typeName)
-              code.typeName
+        override def getPackedType(dslType: DslType[_]): Fastring = {
+          dslType.flatten match {
+            case Seq(atom) => fast"$atom"
+            case flattenTypes =>
+              val identifier = fast"tuple_${flattenTypes.mkFastring("_")}"
+              if (!types(flattenTypes)) {
+                types += flattenTypes
+                globalDeclarations += fast"struct $identifier;"
+                globalDefinitions += fastraw"""
+struct $identifier {
+  ${(for ((t, i) <- flattenTypes.view.zipWithIndex) yield {
+                  fast"\n  $t _$i;"
+                }).mkFastring}
+};"""
+              }
+              fast"struct $identifier"
           }
         }
 
-        override def getValue(dslFunction: DslFunction[_, _]): Fastring = {
+        override def getValue(dslFunction: DslFunction[_, _]): Value = {
           functionCodeCache.getOrElseUpdate(
             dslFunction, {
               val code = dslFunction.toCode(this)
@@ -93,15 +101,21 @@ object OpenCL {
       val outputId = (for (i <- (0 until numberOfDimensions).view) yield fast"start${i}_$indexId").mkFastring("+")
 
       val result = functionContext.getValue(dslFunction)
-      val outputTypeName = functionContext.getType(outputType)
-      val inputTypeName = functionContext.getType(inputType)
+      val outputTypeName = functionContext.getPackedType(outputType)
+      val inputTypeName = functionContext.getPackedType(inputType)
+
+      val inputParameters = for ((inputType, i) <- inputType.flatten.view.zipWithIndex) yield {
+        fast"$inputType __input_$i"
+      }
       val outputValueName = raw"""output_${nextId()}"""
+      val outputParameter = fast"__global $outputTypeName * $outputValueName"
+      val allParameters = inputParameters :+ outputParameter
       fastraw"""
-__kernel void $functionName(/*$inputTypeName input, */ __global $outputTypeName * $outputValueName) {
+__kernel void $functionName(${allParameters.mkFastring(", ")}) {
   ${localDefinitions.mkFastring}
   ${sizes.mkFastring}
   ${starts.mkFastring}
-  $outputValueName[$outputId] = $result;
+  $outputValueName[$outputId] = ${result.packed};
 }
 """
     }
@@ -118,15 +132,37 @@ ${exportedFunctions.mkFastring}
     final case class Code(globalDeclarations: Fastring = Fastring.empty,
                           globalDefinitions: Fastring = Fastring.empty,
                           localDefinitions: Fastring = Fastring.empty,
-                          value: Fastring)
+                          value: Value)
+    trait Value {
+      def unpacked: Seq[Fastring]
+      def packed: Fastring
+    }
+
+    final case class Atom(value: Fastring) extends Value {
+      override def packed: Fastring = value
+      override def unpacked = Seq(value)
+    }
+
+    final case class Unpacked(unpacked: Seq[Fastring]) extends Value {
+      override def packed = fast"{ ${unpacked.mkFastring(", ")} }"
+    }
+
+    final case class Packed(packed: Fastring, numberOfFields: Int) extends Value {
+      override def unpacked: Seq[Fastring] = {
+        for (i <- 0 until numberOfFields) yield {
+          fast"$packed._$i"
+        }
+      }
+    }
+
     final case class DoubleLiteral(data: Double) extends DslFunction[HList, Double] {
       override def toCode(context: Context): Code = {
-        Code(value = Fastring(data))
+        Code(value = Atom(value = Fastring(data)))
       }
     }
     final case class FloatLiteral(data: Float) extends DslFunction[HList, Float] {
       override def toCode(context: Context): Code = {
-        Code(value = fast"${data}f")
+        Code(value = Atom(value = fast"${data}f"))
       }
     }
 
@@ -136,11 +172,13 @@ ${exportedFunctions.mkFastring}
         extends DslFunction[Input, Output] {
       override def toCode(context: Context): Code = {
         val name = context.freshName("plus")
+        val flattenType = dslType.flatten
+        val packedType = context.getPackedType(dslType)
         Code(
           localDefinitions = fastraw"""
-  ${context.getType(dslType)} $name = ${context.getValue(operand1)} + ${context.getValue(operand2)};
+  $packedType $name = ${context.getValue(operand1).packed} + ${context.getValue(operand2).packed};
 """,
-          value = Fastring(name)
+          value = Packed(Fastring(name), flattenType.length)
         )
       }
     }
@@ -153,51 +191,22 @@ ${exportedFunctions.mkFastring}
   }
 
   object DslType {
-    final case class Code(globalDeclarations: Fastring = Fastring.empty,
-                          globalDefinitions: Fastring = Fastring.empty,
-                          identifier: String,
-                          typeName: Fastring)
 
-    object Code {
-      def builtInType(identifier: String) = {
-        new Code(identifier = identifier, typeName = Fastring(identifier))
-      }
-
-      def structType(definitionBody: Fastring, identifier: String) = {
-        new Code(
-          globalDeclarations = fast"struct $identifier;",
-          globalDefinitions = fast"""
-struct $identifier {
-  $definitionBody
-};""",
-          identifier = identifier,
-          typeName = fast"struct $identifier"
-        )
-
-      }
+    implicit object DslHNil extends DslType[HNil] {
+      override def flatten = Nil
     }
 
-    object DslHNil extends DslType[HNil] {
-      override def toCode(context: Context): Code = {
-        Code.structType(fast"", "HNil")
-      }
+    implicit object DslDouble extends DslType[Double] {
+      override val flatten = mutable.WrappedArray.make(Array("double"))
     }
 
-    object DslDouble extends DslType[Double] {
-      override def toCode(context: Context): Code = {
-        Code.builtInType("double")
-
-      }
-    }
-    object DslFloat extends DslType[Float] {
-      override def toCode(context: Context): Code = {
-        Code.builtInType("float")
-      }
+    implicit object DslFloat extends DslType[Float] {
+      override val flatten = mutable.WrappedArray.make(Array("float"))
     }
 
   }
 
   trait DslType[NativeType] {
-    def toCode(context: Context): DslType.Code
+    def flatten: Seq[String]
   }
 }
