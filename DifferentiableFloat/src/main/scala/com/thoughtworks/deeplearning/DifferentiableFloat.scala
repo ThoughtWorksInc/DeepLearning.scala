417d3123f721504555150169cd8f943edd94c44c
张志豪 <izhangzhihao@hotmail.com>
2017-03-22 16:18:45 +0800

    add methods for DifferentiableFloat

--- DifferentiableFloat/src/main/scala/com/thoughtworks/deeplearning/DifferentiableFloat.scala
+++ DifferentiableFloat/src/main/scala/com/thoughtworks/deeplearning/DifferentiableFloat.scala
@@ -1,10 +1,14 @@
 package com.thoughtworks.deeplearning
 
+import cats.kernel.CommutativeGroup
 import com.qifun.statelessFuture.Future
 import com.thoughtworks.deeplearning.CumulativeTape.MonoidTape
 import com.thoughtworks.deeplearning.Layer.Tape
+import shapeless.the
 
 /**
+  * A namespace of common operators for Double layers.
+  *
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
 object DifferentiableFloat {
@@ -33,16 +37,185 @@ object DifferentiableFloat {
         upstream1.backward(delta).await
       }
 
-      override implicit protected def monoid = cats.instances.float.catsKernelStdGroupForFloat
+      override implicit protected def monoid: CommutativeGroup[Float] = cats.instances.float.catsKernelStdGroupForFloat
+    }
+
+    final class Negative(upstream: Tape.Aux[Float, Float]) extends CumulativeTape with MonoidTape {
+
+      override type Data = Float
+      override type Delta = Float
+
+      override protected def closeUpstreams(): Future[Unit] = Future {
+        upstream.close().await
+      }
+
+      /**
+        * Performs the underlying backward pass with all `outputDelta`s that previously received from [[forceBackward]].
+        */
+      override protected def rawBackward(delta: Float): Future[Unit] = Future {
+        upstream.backward(-delta)
+      }
+
+      override implicit protected def monoid: CommutativeGroup[Float] = cats.instances.float.catsKernelStdGroupForFloat
+
+      override def isTrainable: Boolean = upstream.isTrainable
+
+      override def value: Float = -upstream.value
+    }
+
+    final class Reciprocal(upstream: Tape.Aux[Float, Float]) extends CumulativeTape with MonoidTape {
+      override type Data = Float
+      override type Delta = Float
+
+      override protected def closeUpstreams(): Future[Unit] = Future {
+        upstream.close().await
+      }
+
+      /**
+        * Performs the underlying backward pass with all `outputDelta`s that previously received from [[forceBackward]].
+        */
+      override protected def rawBackward(delta: Float): Future[Unit] = Future {
+        val a = upstream.value
+        upstream.backward(-delta / (a * a))
+      }
+
+      override implicit protected def monoid: CommutativeGroup[Float] = cats.instances.float.catsKernelStdGroupForFloat
+
+      override def isTrainable: Boolean = upstream.isTrainable
+
+      override def value: Float = the[Numeric[Float]].one / upstream.value
+    }
+
+    final class Substract(upstream0: Tape.Aux[Float, Float], upstream1: Tape.Aux[Float, Float])
+        extends CumulativeTape
+        with MonoidTape {
+
+      override type Data = Float
+      override type Delta = Float
+
+      override protected def closeUpstreams(): Future[Unit] = Future {
+        upstream0.close().await
+        upstream1.close().await
+      }
+
+      /**
+        * Performs the underlying backward pass with all `outputDelta`s that previously received from [[forceBackward]].
+        */
+      override protected def rawBackward(delta: Float): Future[Unit] = Future {
+        upstream0.backward(delta)
+        upstream1.backward(-delta)
+      }
+
+      override implicit protected def monoid: CommutativeGroup[Float] = cats.instances.float.catsKernelStdGroupForFloat
+
+      override def isTrainable: Boolean = upstream0.isTrainable || upstream1.isTrainable
+
+      override def value: Float = upstream0.value - upstream1.value
+    }
+
+    final class Times(upstream0: Tape.Aux[Float, Float], upstream1: Tape.Aux[Float, Float])
+        extends CumulativeTape
+        with MonoidTape {
+
+      override type Data = Float
+      override type Delta = Float
+
+      override protected def closeUpstreams(): Future[Unit] = Future {
+        upstream0.close().await
+        upstream1.close().await
+      }
+
+      /**
+        * Performs the underlying backward pass with all `outputDelta`s that previously received from [[forceBackward]].
+        */
+      override protected def rawBackward(delta: Float): Future[Unit] = Future {
+        val a = upstream0.value
+        val b = upstream1.value
+        upstream0.backward(delta * b)
+        upstream1.backward(delta * a)
+      }
+
+      override implicit protected def monoid: CommutativeGroup[Float] = cats.instances.float.catsKernelStdGroupForFloat
+
+      override def isTrainable: Boolean = upstream0.isTrainable || upstream1.isTrainable
+
+      override def value: Float = upstream0.value * upstream1.value
+    }
+
+    final class Log(upstream: Tape.Aux[Float, Float]) extends CumulativeTape with MonoidTape {
+
+      override type Data = Float
+      override type Delta = Float
+
+      override protected def closeUpstreams(): Future[Unit] = Future {
+        upstream.close().await
+      }
+
+      /**
+        * Performs the underlying backward pass with all `outputDelta`s that previously received from [[forceBackward]].
+        */
+      override protected def rawBackward(delta: Float): Future[Unit] = Future {
+        upstream.backward(delta / upstream.value)
+      }
+
+      override implicit protected def monoid: CommutativeGroup[Float] = cats.instances.float.catsKernelStdGroupForFloat
+
+      override def isTrainable: Boolean = upstream.isTrainable
+
+      override def value: Float = math.log(upstream.value).toFloat
+    }
+
+    final class Exp(upstream: Tape.Aux[Float, Float]) extends CumulativeTape with MonoidTape {
+
+      override type Data = Float
+      override type Delta = Float
+
+      override protected def closeUpstreams(): Future[Unit] = Future {
+        upstream.close().await
+      }
+
+      /**
+        * Performs the underlying backward pass with all `outputDelta`s that previously received from [[forceBackward]].
+        */
+      override protected def rawBackward(delta: Float): Future[Unit] = Future {
+        upstream.backward(delta * value)
+      }
+
+      override implicit protected def monoid: CommutativeGroup[Float] = cats.instances.float.catsKernelStdGroupForFloat
+
+      override def isTrainable: Boolean = upstream.isTrainable
+
+      override def value: Float = math.exp(upstream.value).toFloat
     }
   }
 
   import Tapes._
 
   implicit final class FloatTapeOps(floatTape: Tape.Aux[Float, Float]) {
-    def +(right: Tape.Aux[Float, Float]) = {
+    def +(right: Tape.Aux[Float, Float]): Plus = {
       new Plus(floatTape.duplicate(), right.duplicate())
     }
+
+    def - : Negative = {
+      new Negative(floatTape)
+    }
+
+    def *(right: Tape.Aux[Float, Float]): Times = {
+      new Times(floatTape, right)
+    }
+
+    def /(right: Tape.Aux[Float, Float]): Times = {
+      new Times(floatTape, new Reciprocal(right))
+    }
+
+    def log: Log = {
+      new Log(floatTape)
+    }
+
+    def exp: Exp = {
+      new Exp(floatTape)
+    }
+
   }
 
   // implicit helpers, ops, ...
--- DifferentiableFloat/src/test/scala/com/thoughtworks/deeplearning/DifferentiableFloatSpec.scala
+++ DifferentiableFloat/src/test/scala/com/thoughtworks/deeplearning/DifferentiableFloatSpec.scala
@@ -1,11 +1,8 @@
 package com.thoughtworks.deeplearning
 
 import com.qifun.statelessFuture.Future
-import com.thoughtworks.deeplearning.Layer.Tape
-import org.scalatest._
-import DifferentiableFloat._
 import com.qifun.statelessFuture.util.Promise
-import com.thoughtworks.deeplearning.Symbolic.Layers.Literal
+import org.scalatest._
 
 /**
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
@@ -18,29 +15,29 @@ final class DifferentiableFloatSpec extends AsyncFreeSpec with Matchers {
     p
   }
 
-  "xxxx" in toConcurrentFuture {
-//
-//    def layer[Input0, Output0 <: Tape](f:  Input0 => Future[Output0]):Layer.Aux[Input0, Output0] = new Layer {
-//      override type Output = Output0
-//      override type Input = Input0
-//      override def forward(input: Input): Future[Output] = f(input)
-//    }
-//
-//    val myNeuralNetwork2 = layer { input: Tape.Aux[Float, Float] => Future {
-//        val t = input + input
-//        if (t.value > 0.5f) {
-//          t + input
-//        } else {
-//          t
-//        }
-//      }
-//    }
+ /*   "xxxx" in toConcurrentFuture {
+    //
+    //    def layer[Input0, Output0 <: Tape](f:  Input0 => Future[Output0]):Layer.Aux[Input0, Output0] = new Layer {
+    //      override type Output = Output0
+    //      override type Input = Input0
+    //      override def forward(input: Input): Future[Output] = f(input)
+    //    }
+    //
+    //    val myNeuralNetwork2 = layer { input: Tape.Aux[Float, Float] => Future {
+    //        val t = input + input
+    //        if (t.value > 0.5f) {
+    //          t + input
+    //        } else {
+    //          t
+    //        }
+    //      }
+    //    }
 
     val myNeuralNetwork: Layer.Aux[Tape.Aux[Float, Float], Tape.Aux[Float, Float]] = new Layer {
       override def forward(input: Input): Future[Output] = Future {
         val t = input + input
         try {
-          val t2 = (t + t)
+          val t2 = t + t
           try {
             val t3 = t2 + t
             t3
@@ -69,7 +66,31 @@ final class DifferentiableFloatSpec extends AsyncFreeSpec with Matchers {
         l.close().await
       }
     }
-
   }
+
+  "Plus" in toConcurrentFuture {
+    val myNeuralNetwork: Layer.Aux[Tape.Aux[Float, Float], Tape.Aux[Float, Float]] = new Layer {
+      override def forward(input: Input): Future[Output] = Future {
+        input + input
+      }
+
+      override type Output = Tape.Aux[Float, Float]
+      override type Input = Tape.Aux[Float, Float]
+    }
+
+    Future {
+      val input = Literal(1.0f)
+      try {
+        val outputTape = myNeuralNetwork.forward(input).await
+        try {
+          outputTape.backward(1.0f).await should be(2.0f)
+        } finally {
+          outputTape.close().await
+        }
+      } finally {
+        input.close().await
+      }
+    }
+  }*/
 
 }
