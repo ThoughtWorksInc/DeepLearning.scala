96460348a3c047bfe75f94b7a524bb8a98b9bdf9
Yang Bo <atryyang@thoughtworks.com>
2017-04-19 13:51:34 +0800

    Close resources during tests

--- DifferentiableKernel/src/test/scala/com/thoughtworks/deeplearning/DifferentiableKernelSpec.scala
+++ DifferentiableKernel/src/test/scala/com/thoughtworks/deeplearning/DifferentiableKernelSpec.scala
@@ -17,69 +17,85 @@ import scalaz.concurrent.Future
   * @author 杨博 (Yang Bo) &lt;pop.atry@gmail.com&gt;
   */
 class DifferentiableKernelSpec extends AsyncFreeSpec with Matchers {
-  executionContext
-  "*" in {
+  "Given an new OpenCL context" - {
     val platform = OpenCL.platforms.head
 
     val device = platform.devices.maxBy { device =>
       Seq(CL_DEVICE_TYPE_CPU, CL_DEVICE_TYPE_GPU, CL_DEVICE_TYPE_ACCELERATOR).indexOf(device.deviceType)
     }
+    val openclTask = throwableMonadic[RAIITask] {
+      val supportedProperties = device.queueProperties
 
-    val Array(supportedProperties) = {
-      val propertiesBuffer = Array[Long](0L)
-      clGetDeviceInfo(device.id, CL_DEVICE_QUEUE_PROPERTIES, propertiesBuffer, null) match {
-        case CL_SUCCESS =>
-          propertiesBuffer
-        case errorCode =>
-          throw new IllegalStateException(s"clGetDeviceInfo error: $errorCode")
-      }
-    }
-    val context = { () =>
-      platform.createContext({ (errorInfo, data) =>
-        println(errorInfo)
-      }, device)
-    }.apply()
-    val commandQueue =
-      context.createCommandQueue(
-        device,
-        Map(
-          CL_QUEUE_PROPERTIES -> {
-            {
-              CL10.CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE & supportedProperties
-            } | {
-              if (device.capabilities.OpenCL20) {
-                CL_QUEUE_ON_DEVICE
-              } else {
-                0
+      val context = RAIITask
+        .managed(platform.createContext({ (errorInfo, data) =>
+          info(errorInfo)
+        }, device))
+        .each
+
+      val commandQueue = RAIITask
+        .managed(
+          context.createCommandQueue(
+            device,
+            Map(
+              CL_QUEUE_PROPERTIES -> {
+                {
+                  CL10.CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE & supportedProperties
+                } | {
+                  if (device.capabilities.OpenCL20) {
+                    CL_QUEUE_ON_DEVICE
+                  } else {
+                    0
+                  }
+                }
               }
-            }
+            )
+          ))
+        .each
+
+      (context, commandQueue)
+    }
+
+    "When fill a buffer with 42.0f" - {
+      val differentiableKernel = {
+        import OpenCLLayer._
+        floatLiteral(42.0f)
+      }
+      import DifferentiableKernel._
+
+      val resultTask = openclTask
+        .flatMap {
+          case (context, commandQueue) =>
+            RAIITask.unmanaged(
+              RAIITask.run(
+                throwableMonadic[RAIITask] {
+                  val layer = differentiableKernel.compile(context, device, commandQueue).each
+                  val outputTape = layer(1, Map.empty).each
+                  val delta = RAIITask.managed(context.createBuffer[Float](1))
+                  RAIITask.unmanaged(outputTape.backward(delta)).each
+                  val f = BufferUtils.createFloatBuffer(1)
+                  val event = RAIITask.managed(commandQueue.enqueueReadBuffer(outputTape.data, f)).each
+                  RAIITask.unmanaged(event.waitForComplete()).each
+                  f
+                }
+              )
+            )
+        }
+        .run
+        .run
+      "The content should be 42.0f" in {
+        val p = Promise[Assertion]
+        resultTask
+          .map(_.map { f =>
+            f.capacity should be(1)
+            f.get(0) should be(42.0f)
+
+          })
+          .unsafePerformAsync { either =>
+            p.complete(scalaz.std.`try`.fromDisjunction(either))
           }
-        )
-      )
-    val differentiableKernel = {
-      OpenCLLayer.floatLiteral(42.0f)
+        p.future
+      }
+
     }
-    import DifferentiableKernel._
-
-    val layerTask: RAIITask[(Int, Map[Any, Tape]) => RAIITask[Tape.Aux[OpenCL.Buffer[Float], OpenCL.Buffer[Float]]]] =
-      differentiableKernel.compile(context, device, commandQueue)
-
-    val p = Promise[Assertion]
-    throwableMonadic[RAIITask] {
-      val layer = layerTask.each
-      val outputTape = layer(1, Map.empty).each
-      val delta = RAIITask.managed(context.createBuffer[Float](1))
-      RAIITask.unmanaged(outputTape.backward(delta)).each
-      val f = BufferUtils.createFloatBuffer(1)
-      val event = RAIITask.managed(commandQueue.enqueueReadBuffer(outputTape.data, f)).each
-      RAIITask.unmanaged(event.waitForComplete()).each
-      f.capacity should be(1)
-      f.get(0) should be(42.0f)
-    }.run.run.unsafePerformAsync { either =>
-      p.complete(scalaz.std.`try`.fromDisjunction(either))
-    }
-
-    p.future
   }
-
 }
