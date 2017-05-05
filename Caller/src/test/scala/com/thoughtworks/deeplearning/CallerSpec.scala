3580f8e4117877b54c40751a165c8e0db7bc8cfe
Yang Bo <atryyang@thoughtworks.com>
2017-05-05 12:09:35 +0800

    Top-level object name should be the same as the file name

--- Caller/src/test/scala/com/thoughtworks/deeplearning/CallerSpec.scala
+++ Caller/src/test/scala/com/thoughtworks/deeplearning/CallerSpec.scala
@@ -1,14 +1,16 @@
 package com.thoughtworks.deeplearning
 
 import org.scalatest.{FreeSpec, Matchers}
-
-object Foo {
-  def call(implicit caller: Caller[_]): String = {
-    caller.value.getClass.getName
+object CallerSpec {
+  object Foo {
+    def call(implicit caller: Caller[_]): String = {
+      caller.value.getClass.getName
+    }
   }
 }
 
-class CallerSpec extends FreeSpec with Matchers {
+final class CallerSpec extends FreeSpec with Matchers {
+  import CallerSpec._
   "className" in {
     val className: String = Foo.call
     className should be(this.getClass.getName)
