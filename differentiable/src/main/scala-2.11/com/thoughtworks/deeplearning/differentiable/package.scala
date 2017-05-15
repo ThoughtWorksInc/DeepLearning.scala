429e4c041a1a95bbf54ff5904542b6d5df369677
Yang Bo <atryyang@thoughtworks.com>
2017-05-15 09:23:23 +0800

    Generate Scaladoc for type aliases

--- differentiable/src/main/scala-2.11/com/thoughtworks/deeplearning/differentiable/package.scala
+++ differentiable/src/main/scala-2.11/com/thoughtworks/deeplearning/differentiable/package.scala
@@ -3,8 +3,16 @@ package com.thoughtworks.deeplearning
 import com.thoughtworks.raii.asynchronous.Do
 
 package object differentiable {
+
+  /** @template */
   type INDArray = Do[Tape[org.nd4j.linalg.api.ndarray.INDArray, org.nd4j.linalg.api.ndarray.INDArray]]
+
+  /** @template */
   type Float = Do[Tape[scala.Float, scala.Float]]
+
+  /** @template */
   type Double = Do[Tape[scala.Double, scala.Double]]
-  type Any = Do[ Tape[scala.Any, scala.Nothing]]
+
+  /** @template */
+  type Any = Do[Tape[scala.Any, scala.Nothing]]
 }
--- differentiable/src/main/scala-2.12/com/thoughtworks/deeplearning/differentiable/package.scala
+++ differentiable/src/main/scala-2.12/com/thoughtworks/deeplearning/differentiable/package.scala
@@ -2,8 +2,14 @@ package com.thoughtworks.deeplearning
 
 import com.thoughtworks.raii.asynchronous.Do
 
-package object differentiable{
+package object differentiable {
+
+  /** @template */
   type Float = Do[Tape[scala.Float, scala.Float]]
+
+  /** @template */
   type Double = Do[Tape[scala.Double, scala.Double]]
-  type Any = Do[ Tape[scala.Any, scala.Nothing]]
+
+  /** @template */
+  type Any = Do[Tape[scala.Any, scala.Nothing]]
 }
