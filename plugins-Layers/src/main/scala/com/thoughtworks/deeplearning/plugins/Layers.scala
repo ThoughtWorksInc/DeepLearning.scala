8cb92a4414352ccd7e04fb8fdd21ae5e873b4162
张志豪 <izhangzhihao@hotmail.com>
2017-07-20 18:53:10 +0530

    Remove unused imports in Layers.scala

--- plugins-Layers/src/main/scala/com/thoughtworks/deeplearning/plugins/Layers.scala
+++ plugins-Layers/src/main/scala/com/thoughtworks/deeplearning/plugins/Layers.scala
@@ -1,19 +1,7 @@
 package com.thoughtworks.deeplearning
 package plugins
-import java.util.logging.Logger
-
 import com.thoughtworks.deeplearning.DeepLearning.Tape
-import com.thoughtworks.feature.{Factory, ImplicitApply, PartialApply, The}
-import com.thoughtworks.feature.Factory.inject
 import com.thoughtworks.raii.asynchronous.Do
-import com.thoughtworks.raii.asynchronous.Do._
-import com.thoughtworks.raii.shared._
-import shapeless.{Poly1, Poly2}
-import shapeless.poly.Case1
-
-import scalaz.syntax.all._
-import scala.annotation.meta.getter
-import scalaz.concurrent.Task
 
 /** A plugin that enables [[Layer]] in neural networks. */
 trait Layers {
